724ae6d52c01ebc4ba2b6aa39e4307bfd87aff55
talhakhan2180 <67999884+talhakhan2180@users.noreply.github.com>
2020-12-23 13:06:41 +0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+int URDU;
+ int English;
+  int PAK;
+   int TOTAL;
+   int AVERAGE;
+
+    TOTAL=3;
+    AVERAGE=English+URDU+PAK/TOTAL;
+
+    cout<<"ENTER OBTAINED  English MARKS=";
+    cin>>English;
+cout<< endl;
+
+    cout<<"ENTER OBTAINED URDUMARKS=";
+    cin>>URDU;
+cout<< endl;
+
+
+  cout<<"ENTER OBTAINED PAK MARKS=";
+    cin>>PAK;
+cout<< endl;
+
+
+  cout<<"AVERAGE MARKS=";
+  cout<<AVERAGE;
+
+cout<< endl;
+    return 0;
+}
